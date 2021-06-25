46dad71f9ef64325919d5d1ff2181543aadd3e73
SAW AIK HONG <sawaikhong20@gmail.com>
2021-06-25 17:24:00 +0800

    my api

--- index.js
+++ index.js
@@ -171,16 +171,3 @@ app.listen(port, () => {
     collection_2 = database.collection(COLLECTION_NAME_2);
     });
 });
-
-/*
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-*/
